c89ff4bbea946400023c46eeb7d352d404a8ae43
Shinya Uryu <suika1127@gmail.com>
2019-10-25 19:00:00 +0900

    [pkg] Add renv

--- 01-setup.R
+++ 01-setup.R
@@ -1,5 +1,8 @@
 ####################################
 # プロジェクトを作成
 ####################################
-library(renv)
+library(renv, warn.conflicts = FALSE)
 activate() # Commit
+status() # renvがlockファイルに記載されていないので追加する
+
+snapshot(confirm = FALSE)
