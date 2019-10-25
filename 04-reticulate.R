e8ef93dded820b5648b35696e2e23a2c92204681
Shinya Uryu <suika1127@gmail.com>
2019-10-25 21:09:09 +0900

    Create conda environment

--- 04-reticulate.R
+++ 04-reticulate.R
@@ -4,3 +4,22 @@ py_config()
 repl_python()
 
 renv::snapshot(confirm = FALSE)
+
+# environment.ymlを用意 --> Conda環境にPythonライブラリが追加される
+renv::use_python(type = "conda", name = "talk_191026_tokyor82")
+renv::restore()
+# conda activate talk_191026_tokyor82
+# conda update --all
+renv::snapshot() # environment.ymlも管理するようになる
+
+repl_python()
+# Python 3.7.3 (/Users/uri/miniconda3/envs/talk_191026_tokyor82/bin/python)
+# Reticulate 1.13 REPL -- A Python interpreter in R.
+
+# うまくいかない時もある...
+# conda remove -n talk_191026_tokyor82 --all
+# conda create -n talk_191026_tokyor82
+# conda activate talk_191026_tokyor82
+# conda install -c conda-forge matplotlib gdal geopandas shapely
+# conda install -c conda-forge earthengine-api folium
+# conda update --all
